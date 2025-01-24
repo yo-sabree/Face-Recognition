45abd5ae4ae89221f6fdbde0c282ce882f64c158
Sabareesh M B <137255627+yo-sabree@users.noreply.github.com>
2025-01-24 12:22:44 +0530

    Update FR_Training.py

--- FR_Training.py
+++ FR_Training.py
@@ -1,18 +1,33 @@
-import face_recognition
+import cv2
 import os
-import pickle
+import numpy as np
 
-data = []
-labels = []
-for person_name in os.listdir("dataset"):
-    person_path = f"dataset/{person_name}"
-    for img_name in os.listdir(person_path):
-        img_path = f"{person_path}/{img_name}"
-        image = face_recognition.load_image_file(img_path)
-        encodings = face_recognition.face_encodings(image)
-        if encodings:
-            data.append(encodings[0])
-            labels.append(person_name)
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
 
-with open("face_encodings.pkl", "wb") as f:
-    pickle.dump({"encodings": data, "names": labels}, f)
+def train_model():
+    data_dir = 'Dataset'
+    labels = []
+    faces = []
+    names = {}
+    current_id = 0
+
+    for person_name in os.listdir(data_dir):
+        person_path = os.path.join(data_dir, person_name)
+        if os.path.isdir(person_path):
+            names[current_id] = person_name
+            for img_name in os.listdir(person_path):
+                img_path = os.path.join(person_path, img_name)
+                img = cv2.imread(img_path)
+                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                faces_detected = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+                for (x, y, w, h) in faces_detected:
+                    faces.append(gray[y:y+h, x:x+w])
+                    labels.append(current_id)
+            current_id += 1
+
+    recognizer.train(faces, np.array(labels))
+    recognizer.save('trained_model.yml')
+    np.save('names.npy', names)
+
+train_model()
